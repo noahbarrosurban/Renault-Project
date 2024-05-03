e271ef1391793f097660c43241ad09d3bd8b0f4b
Noah Barros <86192433+noahbarros@users.noreply.github.com>
2024-05-03 20:06:21 -0300

    Inicial commit

---
+++ src/main/kotlin/jornadadeaprendizagem/renault/RenaultApplication.kt
@@ -0,0 +1,11 @@
+package jornadadeaprendizagem.renault
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RenaultApplication
+
+fun main(args: Array<String>) {
+	runApplication<RenaultApplication>(*args)
+}
---
+++ src/test/kotlin/jornadadeaprendizagem/renault/RenaultApplicationTests.kt
@@ -0,0 +1,13 @@
+package jornadadeaprendizagem.renault
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RenaultApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
